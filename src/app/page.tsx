86aae7ab68b13e1453867a41a296bb1921129b82
Pacifique Harerimana <harerimanapacifique95@gmail.com>
2026-08-31 20:46:05 +0200

    feat: limit landing page to video hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { Footer } from "@/components/layout/footer";
 import { Navbar } from "@/components/layout/navbar";
-import { ExploreCategories } from "@/components/sections/explore-categories/explore-categories";
-import { FeaturedListings } from "@/components/sections/featured-listings/featured-listings";
 import { Hero } from "@/components/sections/hero/hero";
-import { HowItWorks } from "@/components/sections/how-it-works/how-it-works";
-import { TrendingLocations } from "@/components/sections/trending-locations/trending-locations";
 
 export default function Home() {
   return (
@@ -12,12 +7,7 @@ export default function Home() {
       <Navbar />
       <main>
         <Hero />
-        <ExploreCategories />
-        <TrendingLocations />
-        <FeaturedListings />
-        <HowItWorks />
       </main>
-      <Footer />
     </>
   );
 }
